0772609e1aceca48a0a9ed2b8f1dbfd29d2fba18
prasangafdz@outlook.com <LykanHypersp0rt@#>
2017-12-03 21:29:23 +0530

    Initial commit.

---
+++ app/src/main/java/com/prasanga/fdo/mysqllistviewtutorial/MainActivity.java
@@ -0,0 +1,31 @@
+package com.prasanga.fdo.mysqllistviewtutorial;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ListView;
+
+
+public class MainActivity extends AppCompatActivity {
+    String url="http://10.0.2.2/ListView/search.php";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ListView lv= (ListView) findViewById(R.id.lv);
+        final Downloader d=new Downloader(this,url,lv);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //EXECUTE DOWNLOAD
+                d.execute();
+            }
+        });
+    }
+}
+
